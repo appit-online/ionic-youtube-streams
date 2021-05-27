723a4015f51843301aed754b8b5421b937423be3
Dave Koller <davidkoller7117@gmail.com>
2021-05-28 00:24:43 +0200

    update dependencies

--- src/lib/cip.service.ts
+++ src/lib/cip.service.ts
@@ -185,6 +185,7 @@ export class CiphService {
 
     // Deleting the `search` part is necessary otherwise changes to
     // `query` won't reflect when running `url.format()`
+    // @ts-ignore
     delete parsedUrl.search;
 
     const query = parsedUrl.query;
--- src/lib/functions/video-info.service.ts
+++ src/lib/functions/video-info.service.ts
@@ -24,6 +24,7 @@ export class VideoInfoService {
         ps: 'default',
         gl: 'US',
         hl: 'en',
+        html5: 1,
       },
     });
 
