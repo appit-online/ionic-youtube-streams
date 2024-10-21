facd79b6dfb6ce4964f858aba959ad1ad94d739e
Dave Koller <davidkoller7117@gmail.com>
2024-10-21 04:25:30 +0200

    cleanup

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -7,8 +7,6 @@ export async function searchVideo(this: any, youtubeId: string, sortType: string
   const httpClient = new HTTP();
   const tubeService = new YTubeService(httpClient);
 
-
-  const watchPageURL = tubeService.BASE_URL ;
   const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
   };
@@ -36,41 +34,40 @@ export async function searchVideo(this: any, youtubeId: string, sortType: string
   // get json from html
   const info = { page: 'watch', player_response: '', response: '', html5player: '' };
   try {
-    try {
-      info.player_response =
-          tubeService.tryParseBetween(body, 'var ytInitialPlayerResponse = ', '}};', '', '}}') ||
-          tubeService.tryParseBetween(body, 'var ytInitialPlayerResponse = ', ';var') ||
-          tubeService.tryParseBetween(body, 'var ytInitialPlayerResponse = ', ';</script>') ||
-          tubeService.findJSON('watch.html', 'player_response', body, /\bytInitialPlayerResponse\s*=\s*\{/i, '</script>', '{', tubeService);
-    } catch (_e) {
-      const args = tubeService.findJSON('watch.html', 'player_response', body, /\bytplayer\.config\s*=\s*{/, '</script>', '{', tubeService);
-      info.player_response = tubeService.findPlayerResponse('watch.html', args, tubeService);
+      try {
+        info.player_response =
+            tubeService.tryParseBetween(body, 'var ytInitialPlayerResponse = ', '}};', '', '}}') ||
+            tubeService.tryParseBetween(body, 'var ytInitialPlayerResponse = ', ';var') ||
+            tubeService.tryParseBetween(body, 'var ytInitialPlayerResponse = ', ';</script>') ||
+            tubeService.findJSON('watch.html', 'player_response', body, /\bytInitialPlayerResponse\s*=\s*\{/i, '</script>', '{', tubeService);
+      } catch (_e) {
+        const args = tubeService.findJSON('watch.html', 'player_response', body, /\bytplayer\.config\s*=\s*{/, '</script>', '{', tubeService);
+        info.player_response = tubeService.findPlayerResponse('watch.html', args, tubeService);
+      }
+
+      info.response =
+          tubeService.tryParseBetween(body, 'var ytInitialData = ', '}};', '', '}}') ||
+          tubeService.tryParseBetween(body, 'var ytInitialData = ', ';</script>') ||
+          tubeService.tryParseBetween(body, 'window["ytInitialData"] = ', '}};', '', '}}') ||
+          tubeService.tryParseBetween(body, 'window["ytInitialData"] = ', ';</script>') ||
+          tubeService.findJSON('watch.html', 'response', body, /\bytInitialData("\])?\s*=\s*\{/i, '</script>', '{', tubeService);
+      // @ts-ignore
+      info.html5player = tubeService.getHTML5player(body);
+    } catch (_) {
+      throw Error(
+          'Error when parsing watch.html, maybe YouTube made a change: ' + body
+      );
     }
 
-    info.response =
-        tubeService.tryParseBetween(body, 'var ytInitialData = ', '}};', '', '}}') ||
-        tubeService.tryParseBetween(body, 'var ytInitialData = ', ';</script>') ||
-        tubeService.tryParseBetween(body, 'window["ytInitialData"] = ', '}};', '', '}}') ||
-        tubeService.tryParseBetween(body, 'window["ytInitialData"] = ', ';</script>') ||
-        tubeService.findJSON('watch.html', 'response', body, /\bytInitialData("\])?\s*=\s*\{/i, '</script>', '{', tubeService);
-    // @ts-ignore
-    info.html5player = tubeService.getHTML5player(body);
-  } catch (_) {
-    throw Error(
-        'Error when parsing watch.html, maybe YouTube made a change: ' + body
-    );
-  }
+    // verify response
+    const playErr = tubeService.playError(info.player_response);
+    if (playErr) {
+      throw playErr;
+    }
 
-  // verify response
-  const playErr = tubeService.playError(info.player_response);
-  if (playErr) {
-    throw playErr;
-  }
-
-  // fetch formats and more
-  const infoResponse = await tubeService.gotConfig(youtubeId, null, info, tubeService, httpClient);
-  return infoResponse
-  }catch (e) {
-    console.log(e)
+    // fetch formats and more
+    const infoResponse = await tubeService.gotConfig(youtubeId, null, info, tubeService, httpClient);
+    return infoResponse
+  } catch (e) {
   }
 }
